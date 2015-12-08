8e0c8e1706a95d40a4dc481fbe9d3f3f11d7bc1c
clydehsieh <clydehsieh0601@gmail.com>
2015-12-08 15:26:06 +0800

    2015

---
+++ AnyWall-iOS/AnyWall/CChave30cm.h
@@ -0,0 +1,13 @@
+//
+//  CChave30cm.h
+//  Anywall
+//
+//  Created by Chin-Hui Hsieh  on 12/8/15.
+//  Copyright © 2015 Parse Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CChave30cm : UITableViewCell
+
+@end
